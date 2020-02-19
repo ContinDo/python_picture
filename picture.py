9f8c8a5f76f12d7e6ddb36b86881dbf9db9e3c4d
zhaoshuzhen <987644525@qq.com>
2020-02-19 16:56:23 +0800

    fix: 实现爬取花瓣网图片简单爬取

---
+++ picture.py
@@ -0,0 +1,24 @@
+import requests
+import re
+import json
+
+content = requests.get('https://huaban.com/discovery/beauty/').text
+startIndex = re.search('\[\"pins\"\] \=\ ', content).span()[1]
+endIndex = re.search('}}];',content[startIndex:]).span()[1] + startIndex - 1
+# print(startIndex,endIndex)
+
+jsonStr = content[startIndex:endIndex]
+jsonObj = json.loads(jsonStr)
+
+# print(len(jsonObj))
+for item in jsonObj:
+    url = 'http://hbimg.huabanimg.com/' + item['file']['key']
+    # print(url)
+    image = requests.get(url).content
+    with open(item['file']['key'] + '.jpg','wb') as file:
+        file.write(image)
+
+
+
+
+
